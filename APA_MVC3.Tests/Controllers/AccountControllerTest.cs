c563956e7ffb8957ca0daa5bc2cf401d6cad9502
unknown <Kurisu@Kurisu-PC.(none)>
2012-04-06 20:58:13 +1000

    The start

---
+++ APA_MVC3.Tests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,452 @@
+﻿using System;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using NUnit.Framework;
+using APA_MVC3;
+using APA_MVC3.Controllers;
+using APA_MVC3.Models;
+
+namespace APA_MVC3.Tests.Controllers
+{
+
+    [TestFixture]
+    public class AccountControllerTest
+    {
+
+        [Test]
+        public void ChangePassword_Get_ReturnsView()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+
+            // Act
+            ActionResult result = controller.ChangePassword();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            Assert.That(((ViewResult)result).ViewData["PasswordLength"], Is.EqualTo(10));
+        }
+
+        [Test]
+        public void ChangePassword_Post_ReturnsRedirectOnSuccess()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            ChangePasswordModel model = new ChangePasswordModel()
+            {
+                OldPassword = "goodOldPassword",
+                NewPassword = "goodNewPassword",
+                ConfirmPassword = "goodNewPassword"
+            };
+
+            // Act
+            ActionResult result = controller.ChangePassword(model);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(RedirectToRouteResult)));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.That(redirectResult.RouteValues["action"], Is.EqualTo("ChangePasswordSuccess"));
+        }
+
+        [Test]
+        public void ChangePassword_Post_ReturnsViewIfChangePasswordFails()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            ChangePasswordModel model = new ChangePasswordModel()
+            {
+                OldPassword = "goodOldPassword",
+                NewPassword = "badNewPassword",
+                ConfirmPassword = "badNewPassword"
+            };
+
+            // Act
+            ActionResult result = controller.ChangePassword(model);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.That(model, Is.EqualTo(viewResult.ViewData.Model));
+            Assert.That(controller.ModelState[""].Errors[0].ErrorMessage, Is.EqualTo("The current password is incorrect or the new password is invalid."));
+            Assert.That(viewResult.ViewData["PasswordLength"], Is.EqualTo(10));
+        }
+
+        [Test]
+        public void ChangePassword_Post_ReturnsViewIfModelStateIsInvalid()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            ChangePasswordModel model = new ChangePasswordModel()
+            {
+                OldPassword = "goodOldPassword",
+                NewPassword = "goodNewPassword",
+                ConfirmPassword = "goodNewPassword"
+            };
+            controller.ModelState.AddModelError("", "Dummy error message.");
+
+            // Act
+            ActionResult result = controller.ChangePassword(model);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.That(viewResult.ViewData.Model, Is.EqualTo(model));
+            Assert.That(viewResult.ViewData["PasswordLength"], Is.EqualTo(10));
+        }
+
+        [Test]
+        public void ChangePasswordSuccess_ReturnsView()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+
+            // Act
+            ActionResult result = controller.ChangePasswordSuccess();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+        }
+
+        [Test]
+        public void LogOff_LogsOutAndRedirects()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+
+            // Act
+            ActionResult result = controller.LogOff();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(RedirectToRouteResult)));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.That(redirectResult.RouteValues["controller"], Is.EqualTo("Home"));
+            Assert.That(redirectResult.RouteValues["action"], Is.EqualTo("Index"));
+            // TODO: Mocking Framework
+            Assert.IsTrue(((MockFormsAuthenticationService)controller.FormsService).SignOut_WasCalled);
+        }
+
+        [Test]
+        public void LogOn_Get_ReturnsView()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+
+            // Act
+            ActionResult result = controller.LogOn();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+        }
+
+        [Test]
+        public void LogOn_Post_ReturnsRedirectOnSuccess_WithoutReturnUrl()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "goodPassword",
+                RememberMe = false
+            };
+
+            // Act
+            ActionResult result = controller.LogOn(model, null);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(RedirectToRouteResult)));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.That(redirectResult.RouteValues["controller"], Is.EqualTo("Home"));
+            Assert.That(redirectResult.RouteValues["action"], Is.EqualTo("Index"));
+            // TODO: Mocking Framework
+            Assert.IsTrue(((MockFormsAuthenticationService)controller.FormsService).SignIn_WasCalled);
+        }
+
+        [Test]
+        public void LogOn_Post_ReturnsRedirectOnSuccess_WithLocalReturnUrl()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "goodPassword",
+                RememberMe = false
+            };
+
+            // Act
+            ActionResult result = controller.LogOn(model, "/someUrl");
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(RedirectResult)));
+            RedirectResult redirectResult = (RedirectResult)result;
+            Assert.That(redirectResult.Url, Is.EqualTo("/someUrl"));
+            // TODO: Mocking Framework
+            Assert.IsTrue(((MockFormsAuthenticationService)controller.FormsService).SignIn_WasCalled);
+        }
+
+        [Test]
+        public void LogOn_Post_ReturnsRedirectToHomeOnSuccess_WithExternalReturnUrl()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "goodPassword",
+                RememberMe = false
+            };
+
+            // Act
+            ActionResult result = controller.LogOn(model, "http://malicious.example.net");
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(RedirectToRouteResult)));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.That(redirectResult.RouteValues["controller"], Is.EqualTo("Home"));
+            Assert.That(redirectResult.RouteValues["action"], Is.EqualTo("Index"));
+            // TODO: Mocking Framework
+            Assert.IsTrue(((MockFormsAuthenticationService)controller.FormsService).SignIn_WasCalled);
+        }
+
+        [Test]
+        public void LogOn_Post_ReturnsViewIfModelStateIsInvalid()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "goodPassword",
+                RememberMe = false
+            };
+            controller.ModelState.AddModelError("", "Dummy error message.");
+
+            // Act
+            ActionResult result = controller.LogOn(model, null);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.That(viewResult.ViewData.Model, Is.EqualTo(model));
+        }
+
+        [Test]
+        public void LogOn_Post_ReturnsViewIfValidateUserFails()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            LogOnModel model = new LogOnModel()
+            {
+                UserName = "someUser",
+                Password = "badPassword",
+                RememberMe = false
+            };
+
+            // Act
+            ActionResult result = controller.LogOn(model, null);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.That(viewResult.ViewData.Model, Is.EqualTo(model));
+            Assert.That(controller.ModelState[""].Errors[0].ErrorMessage, Is.EqualTo("The user name or password provided is incorrect."));
+        }
+
+        [Test]
+        public void Register_Get_ReturnsView()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+
+            // Act
+            ActionResult result = controller.Register();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            Assert.That(((ViewResult)result).ViewData["PasswordLength"], Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Register_Post_ReturnsRedirectOnSuccess()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            RegisterModel model = new RegisterModel()
+            {
+                UserName = "someUser",
+                Email = "goodEmail",
+                Password = "goodPassword",
+                ConfirmPassword = "goodPassword"
+            };
+
+            // Act
+            ActionResult result = controller.Register(model);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(RedirectToRouteResult)));
+            RedirectToRouteResult redirectResult = (RedirectToRouteResult)result;
+            Assert.That(redirectResult.RouteValues["controller"], Is.EqualTo("Home"));
+            Assert.That(redirectResult.RouteValues["action"], Is.EqualTo("Index"));
+        }
+
+        [Test]
+        public void Register_Post_ReturnsViewIfRegistrationFails()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            RegisterModel model = new RegisterModel()
+            {
+                UserName = "duplicateUser",
+                Email = "goodEmail",
+                Password = "goodPassword",
+                ConfirmPassword = "goodPassword"
+            };
+
+            // Act
+            ActionResult result = controller.Register(model);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.That(viewResult.ViewData.Model, Is.EqualTo(model));
+            Assert.That(controller.ModelState[""].Errors[0].ErrorMessage, Is.EqualTo("Username already exists. Please enter a different user name."));
+            Assert.That(viewResult.ViewData["PasswordLength"], Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Register_Post_ReturnsViewIfModelStateIsInvalid()
+        {
+            // Arrange
+            AccountController controller = GetAccountController();
+            RegisterModel model = new RegisterModel()
+            {
+                UserName = "someUser",
+                Email = "goodEmail",
+                Password = "goodPassword",
+                ConfirmPassword = "goodPassword"
+            };
+            controller.ModelState.AddModelError("", "Dummy error message.");
+
+            // Act
+            ActionResult result = controller.Register(model);
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.That(viewResult.ViewData.Model, Is.EqualTo(model));
+            Assert.That(viewResult.ViewData["PasswordLength"], Is.EqualTo(10));
+        }
+
+        private static AccountController GetAccountController()
+        {
+            RequestContext requestContext = new RequestContext(new MockHttpContext(), new RouteData());
+            AccountController controller = new AccountController()
+            {
+                FormsService = new MockFormsAuthenticationService(),
+                MembershipService = new MockMembershipService(),
+                Url = new UrlHelper(requestContext),
+            };
+            controller.ControllerContext = new ControllerContext()
+            {
+                Controller = controller,
+                RequestContext = requestContext
+            };
+            return controller;
+        }
+
+        private class MockFormsAuthenticationService : IFormsAuthenticationService
+        {
+            public bool SignIn_WasCalled;
+            public bool SignOut_WasCalled;
+
+            public void SignIn(string userName, bool createPersistentCookie)
+            {
+                // verify that the arguments are what we expected
+                Assert.AreEqual("someUser", userName);
+                Assert.IsFalse(createPersistentCookie);
+
+                SignIn_WasCalled = true;
+            }
+
+            public void SignOut()
+            {
+                SignOut_WasCalled = true;
+            }
+        }
+
+        private class MockHttpContext : HttpContextBase
+        {
+            private readonly IPrincipal _user = new GenericPrincipal(new GenericIdentity("someUser"), null /* roles */);
+            private readonly HttpRequestBase _request = new MockHttpRequest();
+
+            public override IPrincipal User
+            {
+                get
+                {
+                    return _user;
+                }
+                set
+                {
+                    base.User = value;
+                }
+            }
+
+            public override HttpRequestBase Request
+            {
+                get
+                {
+                    return _request;
+                }
+            }
+        }
+
+        private class MockHttpRequest : HttpRequestBase
+        {
+            private readonly Uri _url = new Uri("http://mysite.example.com/");
+
+            public override Uri Url
+            {
+                get
+                {
+                    return _url;
+                }
+            }
+        }
+
+        private class MockMembershipService : IMembershipService
+        {
+            public int MinPasswordLength
+            {
+                get { return 10; }
+            }
+
+            public bool ValidateUser(string userName, string password)
+            {
+                return (userName == "someUser" && password == "goodPassword");
+            }
+
+            public MembershipCreateStatus CreateUser(string userName, string password, string email)
+            {
+                if (userName == "duplicateUser")
+                {
+                    return MembershipCreateStatus.DuplicateUserName;
+                }
+
+                // verify that values are what we expected
+                Assert.AreEqual("goodPassword", password);
+                Assert.AreEqual("goodEmail", email);
+
+                return MembershipCreateStatus.Success;
+            }
+
+            public bool ChangePassword(string userName, string oldPassword, string newPassword)
+            {
+                return (userName == "someUser" && oldPassword == "goodOldPassword" && newPassword == "goodNewPassword");
+            }
+        }
+    }
+}
